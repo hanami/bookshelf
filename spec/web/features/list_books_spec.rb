ef859e64450e96721c0232be5efeee284fd12582
Sean Collins <sean@cllns.com>
2016-12-21 12:48:51 -0700

    Displaying Dynamic Data (part 1, without exposure)
    Link: http://hanamirb.org/guides/getting-started/#displaying-dynamic-data

--- spec/web/features/list_books_spec.rb
+++ spec/web/features/list_books_spec.rb
@@ -1,6 +1,14 @@
 require 'features_helper'
 
 describe 'List books' do
+  let(:repository) { BookRepository.new }
+  before do
+    repository.clear
+
+    repository.create(title: 'PoEAA', author: 'Martin Fowler')
+    repository.create(title: 'TDD',   author: 'Kent Beck')
+  end
+
   it 'displays each book on the page' do
     visit '/books'
 
--- spec/web/views/books/index_spec.rb
+++ spec/web/views/books/index_spec.rb
@@ -2,15 +2,34 @@ require 'spec_helper'
 require_relative '../../../../apps/web/views/books/index'
 
 describe Web::Views::Books::Index do
-  let(:exposures) { Hash[foo: 'bar'] }
+  let(:exposures) { Hash[books: []] }
   let(:template)  { Hanami::View::Template.new('apps/web/templates/books/index.html.erb') }
   let(:view)      { Web::Views::Books::Index.new(template, exposures) }
   let(:rendered)  { view.render }
 
-  it 'exposes #foo' do
-    skip 'This is an auto-generated test. Edit it and add your own tests.'
+  it 'exposes #books' do
+    view.books.must_equal exposures.fetch(:books)
+  end
 
-    # Example
-    view.foo.must_equal exposures.fetch(:foo)
+  describe 'when there are no books' do
+    it 'shows a placeholder message' do
+      rendered.must_include('<p class="placeholder">There are no books yet.</p>')
+    end
+  end
+
+  describe 'when there are books' do
+    let(:book1)     { Book.new(title: 'Refactoring', author: 'Martin Fowler') }
+    let(:book2)     { Book.new(title: 'Domain Driven Design', author: 'Eric Evans') }
+    let(:exposures) { Hash[books: [book1, book2]] }
+
+    it 'lists them all' do
+      rendered.scan(/class="book"/).count.must_equal 2
+      rendered.must_include('Refactoring')
+      rendered.must_include('Domain Driven Design')
+    end
+
+    it 'hides the placeholder message' do
+      rendered.wont_include('<p class="placeholder">There are no books yet.</p>')
+    end
   end
 end
