3bbc2c49a2bf7feeaa0b701fbc42280030c424a4
Sean Collins <sean@cllns.com>
2016-12-21 12:50:51 -0700

    Displaying Dynamic Data (part 2, with exposure)
    Link: http://hanamirb.org/guides/getting-started/#displaying-dynamic-data

--- apps/web/controllers/books/index.rb
+++ apps/web/controllers/books/index.rb
@@ -2,7 +2,10 @@ module Web::Controllers::Books
   class Index
     include Web::Action
 
+    expose :books
+
     def call(params)
+      @books = BookRepository.new.all
     end
   end
 end
--- spec/web/views/books/index_spec.rb
+++ spec/web/views/books/index_spec.rb
@@ -1,35 +1,24 @@
 require 'spec_helper'
-require_relative '../../../../apps/web/views/books/index'
+require_relative '../../../../apps/web/controllers/books/index'
 
-describe Web::Views::Books::Index do
-  let(:exposures) { Hash[books: []] }
-  let(:template)  { Hanami::View::Template.new('apps/web/templates/books/index.html.erb') }
-  let(:view)      { Web::Views::Books::Index.new(template, exposures) }
-  let(:rendered)  { view.render }
+describe Web::Controllers::Books::Index do
+  let(:action) { Web::Controllers::Books::Index.new }
+  let(:params) { Hash[] }
+  let(:repository) { BookRepository.new }
 
-  it 'exposes #books' do
-    view.books.must_equal exposures.fetch(:books)
+  before do
+    repository.clear
+
+    @book = repository.create(title: 'TDD', author: 'Kent Beck')
   end
 
-  describe 'when there are no books' do
-    it 'shows a placeholder message' do
-      rendered.must_include('<p class="placeholder">There are no books yet.</p>')
-    end
+  it 'is successful' do
+    response = action.call(params)
+    response[0].must_equal 200
   end
 
-  describe 'when there are books' do
-    let(:book1)     { Book.new(title: 'Refactoring', author: 'Martin Fowler') }
-    let(:book2)     { Book.new(title: 'Domain Driven Design', author: 'Eric Evans') }
-    let(:exposures) { Hash[books: [book1, book2]] }
-
-    it 'lists them all' do
-      rendered.scan(/class="book"/).count.must_equal 2
-      rendered.must_include('Refactoring')
-      rendered.must_include('Domain Driven Design')
-    end
-
-    it 'hides the placeholder message' do
-      rendered.wont_include('<p class="placeholder">There are no books yet.</p>')
-    end
+  it 'exposes all books' do
+    action.call(params)
+    action.exposures[:books].must_equal [@book]
   end
 end
