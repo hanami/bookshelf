216bfc7a365ca8f4a2dfe0df323a56b202034cc4
Sean Collins <sean@cllns.com>
2016-12-21 11:54:20 -0700

    Create a new Hanami project

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source 'https://rubygems.org'
+
+gem 'bundler'
+gem 'rake'
+gem 'hanami',       '~> 0.9'
+gem 'hanami-model', '~> 0.7'
+
+gem 'sqlite3'
+
+group :development do
+  # Code reloading
+  # See: http://hanamirb.org/guides/projects/code-reloading
+  gem 'shotgun'
+end
+
+group :test, :development do
+  gem 'dotenv', '~> 2.0'
+end
+
+group :test do
+  gem 'minitest'
+  gem 'capybara'
+end
+
+group :production do
+  # gem 'puma'
+end
---
+++ config/environment.rb
@@ -0,0 +1,40 @@
+require 'bundler/setup'
+require 'hanami/setup'
+require 'hanami/model'
+require_relative '../lib/bookshelf'
+require_relative '../apps/web/application'
+
+Hanami.configure do
+  mount Web::Application, at: '/'
+
+  model do
+    ##
+    # Database adapter
+    #
+    # Available options:
+    #
+    #  * SQL adapter
+    #    adapter :sql, 'sqlite://db/bookshelf_development.sqlite3'
+    #    adapter :sql, 'postgres://localhost/bookshelf_development'
+    #    adapter :sql, 'mysql://localhost/bookshelf_development'
+    #
+    adapter :sql, ENV['DATABASE_URL']
+
+    ##
+    # Migrations
+    #
+    migrations 'db/migrations'
+    schema     'db/schema.sql'
+  end
+
+  mailer do
+    root 'lib/bookshelf/mailers'
+
+    # See http://hanamirb.org/guides/mailers/delivery
+    delivery do
+      development :test
+      test        :test
+      # production :smtp, address: ENV['SMTP_PORT'], port: 1025
+    end
+  end
+end
