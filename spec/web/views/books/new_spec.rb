504c94aca4a77c7f7fa67bb9640a55cc89fc2fb4
Sean Collins <sean@cllns.com>
2021-06-26 16:10:01 -0600

    Update to reflect hanami/hanami.github.io#306
    See: https://github.com/hanami/hanami.github.io/pull/306/files#diff-ca5fbe58ac5b176e6b1a76ed311f89b9be15000e2e5dd05eb3350e9ba1c60602L938

--- spec/web/views/books/new_spec.rb
+++ spec/web/views/books/new_spec.rb
@@ -1,15 +1,3 @@
-require 'spec_helper'
-require_relative '../../../../apps/web/views/books/new'
-
-class NewBookParams < Hanami::Action::Params
-  params do
-    required(:book).schema do
-      required(:title).filled(:str?)
-      required(:author).filled(:str?)
-    end
-  end
-end
-
 RSpec.describe Web::Views::Books::New do
   let(:params)    { OpenStruct.new(valid?: false, error_messages: ['Title must be filled', 'Author must be filled']) }
   let(:exposures) { Hash[params: params] }
@@ -18,11 +6,8 @@ RSpec.describe Web::Views::Books::New do
   let(:rendered)  { view.render }
 
   it 'displays list of errors when params contains errors' do
-    params.valid? # trigger validations
-
     expect(rendered).to include('There was a problem with your submission')
     expect(rendered).to include('Title must be filled')
     expect(rendered).to include('Author must be filled')
   end
 end
-
