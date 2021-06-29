63477167539562ee3882a9524f390cf4a4fe6d09
Sean Collins <sean@cllns.com>
2021-06-29 13:19:35 -0600

    Update to reflect hanami/hanami#860
    See: https://github.com/hanami/hanami/pull/860

--- spec/web/views/books/create_spec.rb
+++ spec/web/views/books/create_spec.rb
@@ -1,13 +1,10 @@
 RSpec.describe Web::Views::Books::Create, type: :view do
-  let(:exposures) { Hash[foo: 'bar'] }
+  let(:exposures) { Hash[format: :html] }
   let(:template)  { Hanami::View::Template.new('apps/web/templates/books/create.html.erb') }
   let(:view)      { described_class.new(template, exposures) }
   let(:rendered)  { view.render }
 
-  it 'exposes #foo' do
-    skip 'This is an auto-generated test. Edit it and add your own tests.'
-
-    # Example
-    view.foo.must_equal exposures.fetch(:foo)
+  it 'exposes #format' do
+    expect(view.format).to eq exposures.fetch(:format)
   end
 end
