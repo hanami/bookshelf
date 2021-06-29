7efffdd1bfcff6ae6970fde528f2a6bdaf560789
Sean Collins <sean@cllns.com>
2021-06-29 12:26:30 -0600

    Update to reflect hanami/hanami.github.io#46 (missed in repo creation)
    See: https://github.com/hanami/hanami.github.io/pull/46
    
    It was before this repo was created, so it seems like I missed this when creating
    this repo.

--- spec/web/controllers/books/index_spec.rb
+++ spec/web/controllers/books/index_spec.rb
@@ -1,9 +1,21 @@
 RSpec.describe Web::Controllers::Books::Index, type: :action do
   let(:action) { described_class.new }
   let(:params) { Hash[] }
+  let(:repository) { BookRepository.new }
+
+  before do
+    repository.clear
+
+    @book = repository.create(title: 'TDD', author: 'Kent Beck')
+  end
 
   it 'is successful' do
     response = action.call(params)
     expect(response[0]).to eq(200)
+  end
+
+  it 'exposes all books' do
+    action.call(params)
+    expect(action.exposures[:books]).to eq([@book])
   end
 end
