e92644005f086111673e5072ddb726ebaa08d6cc
Sean Collins <sean@cllns.com>
2021-07-01 10:23:24 -0600

    Add base view files

--- slices/main/lib/main/view/base.rb
+++ slices/main/lib/main/view/base.rb
@@ -1,12 +1,16 @@
-# frozen_string_literal: true
-
 require "hanami/view"
+require_relative "parts"
 
 module Main
   module View
     class Base < Hanami::View
       # This will eventually be automatic via Hanami 2 view integration
       config.inflector = Hanami.application[:inflector]
+
+      config.part_namespace = Parts
+
+      # FIXME: Remove this?
+      config.default_context = Bookshelf::View::Context.new
     end
   end
 end
