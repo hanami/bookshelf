08b04b3ea544955f2b892dabd9d33fdd6876c9d6
Sean Collins <sean@cllns.com>
2021-11-10 09:26:53 -0500

    Upgrade routes syntax for new class-based version
    See hanami/hanami-2-application-template#33 and hanami/hanami#1116

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 # frozen_string_literal: true
 
-Hanami.application.routes do
-  slice :main, at: "/" do
-    root to: "home.index"
-    # FIXME: Change back once resourceful routing is implemented
-    # resources :books, only: %i[index new create]
-    get "books", to: "books.index", as: "books"
-    get "books/new", to: "books.new"
-    post "books", to: "books.create"
+module Bookshelf
+  class Routes < Hanami::Application::Routes
+    define do
+      slice :main, at: "/" do
+        root to: "home.index"
+        # FIXME: Change back once resourceful routing is implemented
+        # resources :books, only: %i[index new create]
+        get "books", to: "books.index", as: "books"
+        get "books/new", to: "books.new"
+        post "books", to: "books.create"
+      end
+    end
   end
 end
