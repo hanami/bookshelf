a2f61b03e068163dc0cb2e678f63cc25af7f36cb
Sean Collins <sean@cllns.com>
2021-11-10 09:24:29 -0500

    Minimize action includes
    See hanami/hanami-2-application-template#32

--- lib/bookshelf/action.rb
+++ lib/bookshelf/action.rb
@@ -1,21 +1,9 @@
-# auto_register: false
 # frozen_string_literal: true
 
 require "json" # required for Hanami::Action::Flash to work
 require "hanami/action"
-require "hanami/action/cookies"
-require "hanami/action/csrf_protection"
-require "hanami/action/session"
 
 module Bookshelf
   class Action < Hanami::Action
-    def self.inherited(klass)
-      super
-
-      # These will need to be sorted by the framework eventually
-      klass.include Hanami::Action::Cookies
-      klass.include Hanami::Action::Session
-      klass.include Hanami::Action::CsrfProtection
-    end
   end
 end
