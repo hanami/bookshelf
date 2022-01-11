37711cda1d5e6a507dcea515ced9b3d2ce8b1e86
Sean Collins <sean@cllns.com>
2022-01-11 16:23:09 -0400

    Add FIXME not about `require`ing an entity

--- spec/main/entities/book_spec.rb
+++ spec/main/entities/book_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require "spec_helper"
-require_relative "../../../slices/main/entities/book"
+require_relative "../../../slices/main/entities/book" # FIXME: shouldn't be necessary
 
 module Main
   module Entities
