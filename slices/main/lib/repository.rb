769cd47846e78152750a1ea068ea250a8464e423
Sean Collins <sean@cllns.com>
2022-01-11 17:12:28 -0400

    Move create command config to BookRepository
    The  pluging wasn't working when it was in the other location

--- slices/main/lib/repository.rb
+++ slices/main/lib/repository.rb
@@ -2,11 +2,6 @@
 
 module Main
   class Repository < Bookshelf::Repository
-    commands :create,
-             use: :timestamps,
-             plugins_options:
-               {timestamps: {timestamps: %i[created_at updated_at]}}
-
     auto_struct true
   end
 end
--- slices/main/repositories/book_repository.rb
+++ slices/main/repositories/book_repository.rb
@@ -3,6 +3,10 @@
 module Main
   module Repositories
     class BookRepository < Repository[:books]
+      commands :create,
+               use: :timestamps,
+               plugins_options:
+                 {timestamps: {timestamps: %i[created_at updated_at]}}
     end
   end
 end
