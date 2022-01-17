0331dd2e4173b80e245b07b2a97d80f31be4e5ba
Sean Collins <sean@cllns.com>
2022-01-17 18:16:18 -0400

    Change where Entities namespace is specified

--- lib/bookshelf/repository.rb
+++ lib/bookshelf/repository.rb
@@ -5,7 +5,5 @@ require "rom-repository"
 module Bookshelf
   class Repository < ROM::Repository::Root
     include Deps[container: "persistence.rom"]
-
-    struct_namespace Entities
   end
 end
--- slices/main/lib/repository.rb
+++ slices/main/lib/repository.rb
@@ -3,5 +3,7 @@
 module Main
   class Repository < Bookshelf::Repository
     auto_struct true
+
+    struct_namespace Entities
   end
 end
